db92cb635596ad7472ffcae8e1dbed1dc7036b8e
GracyDinma <okohagchidinma@gmail.com>
2024-11-20 05:30:58 +0000

    rotate matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+2D matrix that rotate 90 degree clockwise in-place
+"""
+class PrintableMatrix(list):
+    """
+    Custom list class to modify the print behavior of the entire matrix.
+    """
+    def __repr__(self):
+        return "\n".join(str(row) for row in self)
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotate a 2D matrix 90 degrees clockwise in place.
+    """
+    n = len(matrix)
+
+    """ Transpose the matrix."""
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    """ Reverse each row."""
+    for row in matrix:
+        row.reverse()
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,2 @@
+2D matrix
+A projects that implement an in-palace algorithm to rotate an n x n 2D matrix by 90 degrees clockwise
